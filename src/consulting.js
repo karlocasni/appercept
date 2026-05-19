42d846c77faeb9745a06531323a88cd003f0f93f
Gašpar Bodulica <gasparbodulica@Gaspars-MacBook-Air.local>
2026-05-19 09:45:37 +0200

    Enable repeating scroll animation on scroll down and scroll up

--- src/consulting.js
+++ src/consulting.js
@@ -22,7 +22,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('is-visible');
-            observer.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove('is-visible');
         }
     });
 }, observerOpts);
